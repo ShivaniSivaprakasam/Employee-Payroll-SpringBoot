094e03b64f4d4394c0351a858894e581694d7385
shivani <shivaniavanikkarasi@gmail.com>
2026-03-25 15:17:28 +0530

    [Shivani] : Added : UC3-introduce-dto-and-model

---
+++ employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.employeepayrollapp.dto;
+
+public class EmployeePayrollDTO {
+    private String name;
+    private double salary;
+
+    public EmployeePayrollDTO() {
+    }
+
+    public EmployeePayrollDTO(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
